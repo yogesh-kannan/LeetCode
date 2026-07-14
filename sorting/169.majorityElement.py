0ae3a539949b3e66533617c4e1fc8d591fcfba81
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-07-14 16:35:09 +0530

    Implement majority element algorithm in Python

---
+++ sorting/169.majorityElement.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        count,majority = 0,0
+        for i in range(len(nums)):
+            if count == 0 and majority != nums[i]:
+                majority = nums[i]
+                count += 1
+            elif majority == nums[i]:
+                count += 1
+            else:
+                count -= 1
+        return majority
