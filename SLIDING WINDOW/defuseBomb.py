4be0eb3ddfb706cf1c3b30505a31e5db0fd0d1d9
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-08-04 16:36:42 +0530

    Implement decrypt method in defuseBomb.py

---
+++ SLIDING WINDOW/defuseBomb.py
@@ -0,0 +1,16 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n=len(code)
+        ans=[0]*n
+        if k==0: return ans
+        sgn=-1
+        if k>0:
+            sgn=1
+        else:
+            k=-k
+        ans[0]=wsum=sum(code[sgn:sgn*(k+1):sgn])
+        for l in range(1, n):
+            r=(l+k)%n
+            wsum+=-code[sgn*l]+code[sgn*r]
+            ans[sgn*l]=wsum
+        return ans
