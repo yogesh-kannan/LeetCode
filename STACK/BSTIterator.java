72bd14052b47d110655806aac7063f5cbda034b7
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-08-11 15:53:54 +0530

    Implement BSTIterator for in-order traversal

---
+++ STACK/BSTIterator.java
@@ -0,0 +1,35 @@
+public class BSTIterator {
+    
+    private Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null){
+            stack.push(cur);
+            if(cur.left != null)
+                cur = cur.left;
+            else
+                break;
+        }
+    }
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+        TreeNode cur = node;
+        // traversal right branch
+        if(cur.right != null){
+            cur = cur.right;
+            while(cur != null){
+                stack.push(cur);
+                if(cur.left != null)
+                    cur = cur.left;
+                else
+                    break;
+            }
+        }
+        return node.val;
+    }
+}
